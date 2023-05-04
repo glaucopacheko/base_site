4407d56d7baec2719f4706d477c7f724176f981a
Glauco Pacheko <glaucopacheko@gmail.com>
2023-05-03 21:25:08 -0300

    base

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const index = () => {
+    return (
+        <div className=" h-screen flex items-center justify-center">
+            <h1 className=" text-3xl">Glauco Pacheko</h1>
+        </div>
+    )
+}
+
+export default index
